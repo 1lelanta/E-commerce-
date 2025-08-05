774cf98efacf9d01320c88a1f325fdb4040cfb00
Lelisa Tesfaye <kekikoki4@gmail.com>
2025-08-05 20:09:29 +0300

    new change

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,36 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState("shop")
+    const [selectedOpion, setSelectedOption] = useState("More");
+    const navigate = useNavigate();
+
+    const handleDropdownChange = (e) => {
+        const value = e.target.value;
+        setSelectedOption(value);
+
+        // Optional: Navigate to a new route based on the option
+        switch (value) {
+            case "Watch":
+                navigate("/watch");
+                break;
+            case "Shoes":
+                navigate("/shoes");
+                break;
+            case "Socks":
+                navigate("/socks");
+                break;
+            case "Belt":
+                navigate("/belt");
+                break;
+            default:
+                break;
+        }
+    }
 
   return (
     <div className='navbar'>
@@ -20,6 +45,18 @@ const Navbar = () => {
             <li onClick={()=>{setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to ="/womens">Women</Link>{menu ==="womens"? <hr/>: <></>}</li>
             <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none'}} to ="/kids">Kid</Link>{menu ==="kids"? <hr/>: <></>}</li>
         </ul>
+        <div className="drop-down">
+            
+                <select value={selectedOpion} onChange={handleDropdownChange}>
+                    <option value="More"> More</option>
+                    <option value="Watch">Watch</option>
+                    <option value="Shoes">Shoes</option>
+                    <option value="Socks">Socks</option>
+                    <option value="Belt">Belt</option>
+                </select>
+           
+            
+        </div>
         <div className="nav-login-cart">
             <Link to="/login"><button>Login</button></Link>
             <Link to ="/cart"><img src={cart_icon} alt="" /></Link>
